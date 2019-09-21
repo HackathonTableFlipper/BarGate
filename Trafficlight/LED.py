928fa597b0e540ef70421a7e8f7993ea4a50e924
Johannes Ganser <johannes.ganser@ubimax.com>
2019-09-21 16:07:28 +0200

    intendations

--- Trafficlight/LED.py
+++ Trafficlight/LED.py
@@ -12,10 +12,10 @@ GPIO.setup(BLUE,GPIO.OUT)
 GPIO.output(BLUE,0)
 try:
   while (True): 
-  request = raw_input(“RGB—>”) 
-  if (len(request) == 3): 
-    GPIO.output(RED,int(request[0]))
-    GPIO.output(GREEN,int(request[1]))
-    GPIO.output(BLUE,int(request[2]))
+      request = raw_input(“RGB—>”) 
+      if (len(request) == 3): 
+        GPIO.output(RED,int(request[0]))
+        GPIO.output(GREEN,int(request[1]))
+        GPIO.output(BLUE,int(request[2]))
 except KeyboardInterrupt:
 GPIO.cleanup()

6ed45eba1d41091afecdb52f9b88bcb62179fc2d
Adji A Hack <adji.arioputro@gmail.com>
2019-09-21 21:45:42 +0000

    LightBarrier

---
+++ LightBarrier.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+
+RECEIVER_PIN = 24
+
+if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    GPIO.setup(RECEIVER_PIN, GPIO.IN)
+    print(GPIO.input(RECEIVER_PIN))
+

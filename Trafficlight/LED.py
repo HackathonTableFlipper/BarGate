df2b78a081594ab262e262fbc003b655500bf515
Johannes Ganser <johannes.ganser@ubimax.com>
2019-09-21 15:52:10 +0200

    fix pins

--- Trafficlight/LED.py
+++ Trafficlight/LED.py
@@ -2,9 +2,9 @@ import RPi.GPIO as GPIO
 import time
 
 colors = [0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0xFF00FF, 0x00FFFF]
-R = 17
-G = 18
-B = 27
+R = 11
+G = 12
+B = 13
 
 def setup(Rpin, Gpin, Bpin):
     global pins
